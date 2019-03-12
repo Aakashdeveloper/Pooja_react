a95f0ad4376bb83a5ea40839e0183e45657655c3
aakashDeveloper <ahanda205@gmail.com>
2019-03-12 23:04:40 +0530

    a

--- secondapp/src/index.js
+++ secondapp/src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+import Home from './component/App';
+import Posts from './component/Posts';
+import Profile from './component/Profile';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <header>
+                <Link to="/">Home</Link>
+                <Link to="/posts">Post</Link>
+                <Link to="/profile">Profile</Link>
+            </header>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/posts" component={Posts}></Route>
+            <Route path="/profile" component={Profile}></Route>
+        </div>
+    </BrowserRouter>
+    ,document.getElementById('root'));
