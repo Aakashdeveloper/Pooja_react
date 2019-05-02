15d0614aaa77db5090813b54b135829bf30750cc
aakashDeveloper <ahanda205@gmail.com>
2019-05-02 01:39:29 +0100

    a

--- redux1/src/component/App.js
+++ redux1/src/component/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import MoviesList from '../container/movies_list';
 
 class App extends Component{
     render(){
         return(
-            <div> Welcome to redux</div>
+            <MoviesList></MoviesList>
         )
     }
 }
---
+++ redux1/src/container/movies_list.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class MoviesList extends Component{
+
+    componentWillMount(){
+        this.props.moviesList()
+    }
+    renderList = (movies) => {
+        if(movies){
+            return movies.map((data) => {
+                return(
+                    <div>
+                        <h3>{data.name}</h3>
+                    </div>
+                )
+            })
+        }
+    }
+    render(){
+        return(
+            <div>
+                {this.renderList(this.props.moviesdata)}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        moviesdata: state.movies
+    }
+}
+
+export default connect(mapStateToProps, actions)(MoviesList);
